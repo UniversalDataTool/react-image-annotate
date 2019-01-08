0af5412116a82260bd989f7d477e55ff41d711b3
Severin Ibarluzea <seveibar@gmail.com>
2019-01-08 00:26:43 -0500

    checkpoint

--- src/ImageCanvas/index.js
+++ src/ImageCanvas/index.js
@@ -7,6 +7,7 @@ export default () => {
   const canvasEl = useRef(null)
   const image = useRef(null)
   const [imageLoaded, changeImageLoaded] = useState(false)
+  const [shiftKeyIsDown, changeShiftKeyIsDown] = useState(false)
   const mousePosition = useRef({ x: 0, y: 0 })
   const [[tx, ty, sx, sy], changeTransform] = useState([0, 0, 1, 1])
 
@@ -45,6 +46,16 @@ export default () => {
       ih
     )
     context.restore()
+
+    const onKeyDown = e => e.key === "Shift" && changeShiftKeyIsDown(true)
+    const onKeyUp = e => e.key === "Shift" && changeShiftKeyIsDown(false)
+    window.addEventListener("keydown", onKeyDown)
+    window.addEventListener("keyup", onKeyUp)
+
+    return () => {
+      window.removeEventListener("keydown", onKeyDown)
+      window.removeEventListener("keyup", onKeyUp)
+    }
   })
 
   return (
@@ -59,7 +70,9 @@ export default () => {
           (mousePosition.current.y - ty) / sy
         ]
 
-        changeTransform([umx - umx * sx, umy - umy * sy, sx, sy])
+        if (shiftKeyIsDown) {
+          changeTransform([umx - umx * sx, umy - umy * sy, sx, sy])
+        }
       }}
       onWheel={e => {
         const direction = e.deltaY > 0 ? -1 : e.deltaY < 0 ? 1 : 0
@@ -70,8 +83,8 @@ export default () => {
         ]
 
         const [nsx, nsy] = [
-          Math.max(1, Math.min(3, sx * (1 + direction * 0.2))),
-          Math.max(1, Math.min(3, sy * (1 + direction * 0.2)))
+          Math.max(1, Math.min(10, sx * (1 + direction * 0.3))),
+          Math.max(1, Math.min(10, sy * (1 + direction * 0.3)))
         ]
 
         changeTransform([umx - umx * nsx, umy - umy * nsy, nsx, nsy])
