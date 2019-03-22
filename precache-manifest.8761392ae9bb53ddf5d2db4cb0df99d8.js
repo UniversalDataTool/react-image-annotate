5e61e031f3c7d544b04e8be17db6f603d2b37591
Severin Ibarluzea <seveibar@gmail.com>
2019-03-22 10:42:20 -0400

    Updates

---
+++ precache-manifest.8761392ae9bb53ddf5d2db4cb0df99d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3427ca0114d3fdea924",
+    "url": "/react-image-annotate/static/css/main.2a76f603.chunk.css"
+  },
+  {
+    "revision": "b3427ca0114d3fdea924",
+    "url": "/react-image-annotate/static/js/main.a18cb20e.chunk.js"
+  },
+  {
+    "revision": "9aafae87b94098b78bd4",
+    "url": "/react-image-annotate/static/js/runtime~main.3f5c9a4f.js"
+  },
+  {
+    "revision": "52dff88c32f8357033d2",
+    "url": "/react-image-annotate/static/css/2.0c5748a5.chunk.css"
+  },
+  {
+    "revision": "52dff88c32f8357033d2",
+    "url": "/react-image-annotate/static/js/2.ed02a7ed.chunk.js"
+  },
+  {
+    "revision": "5410a3a533353c093375b1b4dc4b217f",
+    "url": "/react-image-annotate/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-image-annotate/precache-manifest.dacd49c77790b5d10b6eb2ebf39e7a62.js"
+  "/react-image-annotate/precache-manifest.8761392ae9bb53ddf5d2db4cb0df99d8.js"
 );
 
 workbox.clientsClaim();
