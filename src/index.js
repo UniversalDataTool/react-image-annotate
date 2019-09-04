260f99c863524f8861d8c9cd7f270771825d91e2
seveibar <seveibar@gmail.com>
2019-09-03 23:15:34 -0400

    added theme to main export

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 // @flow
 
+import React from "react"
 import Annotator from "./Annotator"
+import Theme from "./Theme"
 
-export default Annotator
+export default (props: any) => {
+  return (
+    <Theme>
+      <Annotator {...props} />
+    </Theme>
+  )
+}
