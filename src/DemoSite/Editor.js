a507a71fe8d23a4c22701256f805cb3520b360a1
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-03-24 19:51:51 +0300

    Added new image for testing previous and next image shortcut keys

--- src/DemoSite/Editor.js
+++ src/DemoSite/Editor.js
@@ -52,6 +52,11 @@ export const examples = {
         src:
           "https://images.unsplash.com/photo-1496905583330-eb54c7e5915a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
         name: "hot-dogs-1"
+      },
+      {
+        src:
+          "https://www.bianchi.com/wp-content/uploads/2019/07/YPB17I555K.jpg",
+        name: "bianchi-oltre-xr4"
       }
     ]
   }),
