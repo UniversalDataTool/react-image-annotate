d9bd91fc3326691653aa45641f660770bdbfde37
Severin Ibarluzea <seveibar@gmail.com>
2020-03-26 03:23:07 -0400

    Feature: Keyboard Shortcuts (rebased on master) (#34)
    * Fix rebase onto master and add shortcut features - minor refactoring
    
    * https://github.com/waoai/react-image-annotate/pull/34#discussion_r395197519
    
    * Deleted unnecessery export which cause bug
    
    * https://github.com/waoai/react-image-annotate/pull/34#discussion_r395198790
    
    * Added action/types to defaultShortcuts and added prev-image/next-image to defaultShortcuts too
    
    * Added next and previous image keys
    
    * Now we use 'HEADER_BUTTON_CLICKED' instead of creating new types and reducers for next image and previous image and they added a and d
    
    * Added ','
    
    * Added new image for testing previous and next image shortcut keys
    
    Co-authored-by: Tamay Eser Uysal <tamayeseruysal@gmail.com>

--- src/DemoSite/Editor.js
+++ src/DemoSite/Editor.js
@@ -52,6 +52,11 @@ export const examples = {
         src:
           "https://images.unsplash.com/photo-1496905583330-eb54c7e5915a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
         name: "hot-dogs-1"
+      },
+      {
+        src:
+          "https://www.bianchi.com/wp-content/uploads/2019/07/YPB17I555K.jpg",
+        name: "bianchi-oltre-xr4"
       }
     ]
   }),
@@ -123,13 +128,13 @@ const Editor = ({ onOpenAnnotator, lastOutput }: any) => {
             className="button"
             variant="outlined"
             disabled={Boolean(currentError)}
-            onClick={() =>
+            onClick={() => {
               onOpenAnnotator(
                 selectedExample === "Custom"
                   ? loadSavedInput()
                   : examples[selectedExample]
               )
-            }
+            }}
           >
             Open Annotator
           </Button>
---
+++ src/Shortcuts/index.js
@@ -0,0 +1,129 @@
+import React, { useState, useEffect } from "react"
+import SidebarBoxContainer from "../SidebarBoxContainer"
+import { setIn } from "seamless-immutable"
+import ShortcutField from "./ShortcutField"
+
+const defaultShortcuts = {
+  select: {
+    action: {
+      type: "SELECT_TOOL"
+    },
+    name: "Select Region",
+    key: "Escape"
+  },
+  zoom: {
+    action: {
+      type: "SELECT_TOOL"
+    },
+    name: "Zoom In/Out",
+    key: "z"
+  },
+  "create-point": {
+    action: {
+      type: "SELECT_TOOL"
+    },
+    name: "Create Point"
+  },
+  "create-box": {
+    action: {
+      type: "SELECT_TOOL"
+    },
+    name: "Add Bounding Box",
+    key: "b"
+  },
+  pan: {
+    action: {
+      type: "SELECT_TOOL"
+    },
+    name: "Pan"
+  },
+  "create-polygon": {
+    action: {
+      type: "SELECT_TOOL"
+    },
+    name: "Create Polygon"
+  },
+  "create-pixel": {
+    action: {
+      type: "SELECT_TOOL"
+    },
+    name: "Create Pixel"
+  },
+  "prev-image": {
+    action: {
+      type: "HEADER_BUTTON_CLICKED",
+      buttonName: "Prev"
+    },
+    name: "Previous Image",
+    key: "a"
+  },
+  "next-image": {
+    action: {
+      type: "HEADER_BUTTON_CLICKED",
+      buttonName: "Next"
+    },
+    name: "Next Image",
+    key: "d"//"ArrowRight"
+  }
+}
+
+export default ({ onShortcutActionDispatched }) => {
+  const [shortcuts, setShortcuts] = useState({}) // useLocalStorage
+
+  useEffect(() => {
+    const newShortcuts = { ...shortcuts }
+    for (const actionId of Object.keys(defaultShortcuts)) {
+      if (!newShortcuts[actionId]) {
+        newShortcuts[actionId] = defaultShortcuts[actionId]
+      }
+    }
+    setShortcuts(newShortcuts)
+  }, [])
+
+  const onChangeShortcut = (actionId, keyName) => {
+    setShortcuts(setIn(shortcuts, [actionId, "key"], keyName))
+  }
+
+  useEffect(() => {
+    const handleKeyPress = e => {
+      for (const actionId in shortcuts) {
+        const shortcut = shortcuts[actionId]
+        if (!shortcut || !shortcut.key) {
+          continue
+        }
+        if (e.key === shortcut.key) {
+          onShortcutActionDispatched({
+            ...shortcut.action,
+            selectedTool: actionId
+          })
+        }
+      }
+    }
+
+    window.addEventListener("keypress", handleKeyPress)
+
+    return () => {
+      window.removeEventListener("keypress", handleKeyPress)
+        document.activeElement.blur()
+    }
+  }, [shortcuts])
+
+  return (
+    <SidebarBoxContainer title="Shortcuts">
+      {Object.keys(shortcuts)
+        .map((actionId, index) => {
+          if (!shortcuts[actionId]) return null
+          return (
+            <ShortcutField
+              key={actionId}
+              actionId={actionId}
+              actionName={shortcuts[actionId].name}
+              keyName={shortcuts[actionId].key || ""}
+              onChangeShortcut={onChangeShortcut}
+            />
+          )
+        })
+        .filter(Boolean)}
+    </SidebarBoxContainer>
+  )
+}
