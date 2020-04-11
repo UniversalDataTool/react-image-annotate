7aee107e0f186b8a82b538a2250540fb6c69aaf6
seveibar <seveibar@gmail.com>
2020-04-11 02:10:17 -0400

    fix: publish dist directory with github workflow

--- .releaserc.js
+++ .releaserc.js
@@ -3,7 +3,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    ["@semantic-release/npm", { npmPublish: true }],
+    ["@semantic-release/npm", { npmPublish: true, pkgRoot: "dist" }],
     "@semantic-release/github",
     [
       "@semantic-release/git",
