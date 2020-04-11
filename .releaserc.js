a3e4fa9e8df7e2b11475310d9e0b829666686264
seveibar <seveibar@gmail.com>
2020-04-11 04:20:08 -0400

    fix: change order, remove optional prop

--- .releaserc.js
+++ .releaserc.js
@@ -3,8 +3,8 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    ["@semantic-release/npm", { npmPublish: false }],
     ["@semantic-release/npm", { npmPublish: true, pkgRoot: "dist" }],
-    ["@semantic-release/npm", { npmPublish: false, pkgRoot: "." }],
     "@semantic-release/github",
     [
       "@semantic-release/git",
