6d6c1b7b756e71d6d2aef5d402b413d5cc40440c
Tamay Eser Uysal <tamayeseruysal@gmail.com>
2020-05-13 23:56:14 +0300

    Made pr prettier

--- src/DemoSite/index.story.js
+++ src/DemoSite/index.story.js
@@ -7,13 +7,12 @@ import { storiesOf } from "@storybook/react"
 import DemoSite from "./"
 
 storiesOf("DemoSite", module)
-    .add("Basic", () => <DemoSite />)
-    .add("Multiple", () =>{
-            return(
-                <React.Fragment>
-                    <DemoSite />
-                    <DemoSite />
-                </React.Fragment>
-            )
-        }
+  .add("Basic", () => <DemoSite />)
+  .add("Multiple", () => {
+    return (
+      <React.Fragment>
+        <DemoSite />
+        <DemoSite />
+      </React.Fragment>
     )
+  })
