37f5b0bd305a3fab529d575524a9c1f57461980e
Florian Duderstadt <flori.duderstadt@gmail.com>
2024-03-20 14:35:16 +0100

    remove ResizePanel

--- src/workspace/SidebarBox/index.jsx
+++ src/workspace/SidebarBox/index.jsx
@@ -1,15 +1,15 @@
 // @flow
 
-import React, {useState, memo, useCallback} from "react"
+import React, { useState, memo, useCallback } from "react"
 import Paper from "@mui/material/Paper"
-import {createTheme, styled, ThemeProvider} from "@mui/material/styles"
+import { createTheme, styled, ThemeProvider } from "@mui/material/styles"
 import ExpandIcon from "@mui/icons-material/ExpandMore"
 import IconButton from "@mui/material/IconButton"
 import Collapse from "@mui/material/Collapse"
 import classnames from "classnames"
 import useEventCallback from "use-event-callback"
 import Typography from "@mui/material/Typography"
-import {useIconDictionary} from "../icon-dictionary.js"
+import { useIconDictionary } from "../icon-dictionary.js"
 import ResizePanel from "../ResizePanel"
 import styles from "./styles.js"
 
@@ -86,15 +86,13 @@ export const SidebarBox = ({
             content
           ) : null
         ) : (
-          <Collapse in={expanded}>
-            <ResizePanel direction="s" style={{height: 100}}>
+          <Collapse in={expanded} >
               <div
                 className="panel"
-                style={{display: "block", overflow: "hidden", height: 500}}
+                style={{ display: "block", overflow: "hidden", height: 150 }}
               >
                 {content}
               </div>
-            </ResizePanel>
           </Collapse>
         )}
       </ContainerDiv>
